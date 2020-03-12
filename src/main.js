4e63c1194f8a54f89daa87a8eefdd5aaf1fae988
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-03-12 09:53:04 +0900

    section-140

--- src/main.js
+++ src/main.js
@@ -6,17 +6,17 @@ Vue.config.productionTip = false
 
 Vue.component('LikeNumber', LikeNumber)
 
-Vue.directive("border", function(el, binding) {
-  el.style.borderWidth = binding.value.width
-  el.style.color = binding.value.color
-  el.style.borderStyle = binding.arg
-  if(binding.modifiers.round) {
-    el.style.borderRadius = "0.5rem"
-  }
-  if(binding.modifiers.shadow) {
-    el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)"
-  }
-})
+// Vue.directive("border", function(el, binding) {
+//   el.style.borderWidth = binding.value.width
+//   el.style.color = binding.value.color
+//   el.style.borderStyle = binding.arg
+//   if(binding.modifiers.round) {
+//     el.style.borderRadius = "0.5rem"
+//   }
+//   if(binding.modifiers.shadow) {
+//     el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)"
+//   }
+// })
 
 // Vue.directive("border", {
 //   // ディレクティブが初めて対象の要素に紐づいた時
