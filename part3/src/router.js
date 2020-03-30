7bcda4eccce7417be677aa76130caa7443d5f220
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-03-30 09:45:08 +0900

    section-203

--- part3/src/router.js
+++ part3/src/router.js
@@ -14,11 +14,14 @@ export default new Router({
   mode: "history",
   routes: [
     {
-    path: '/',
-    components: {
-      default: Home,
-      header: HeaderHome
-    }
+      path: '/',
+      components: {
+        default: Home,
+        header: HeaderHome
+      },
+      beforeEnter(to, from, next) {
+        next(false)
+      }
     },
     {
       path: '/users/:id',
