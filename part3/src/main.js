870fe4ddbdab9d4550a55b498b9d417d0b156b5d
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-03-30 09:42:15 +0900

    section-202

--- part3/src/main.js
+++ part3/src/main.js
@@ -4,6 +4,18 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 認証していない場合とかに使う（ログインしていないとき）
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  console.log(from)
+  console.log(next)
+
+  if (to.path === '/users/1') {
+    next('/')
+  }
+  next()
+})
+
 new Vue({
   router,
   render: h => h(App),
