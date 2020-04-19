dc0a9973a3a680ce0a5fb02e7240b4a50dd5fa37
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-04-19 21:42:44 +0900

    section-209

--- part3/src/views/HeaderHome.vue
+++ part3/src/views/HeaderHome.vue
@@ -5,8 +5,23 @@
       <router-link to="/" class="link" active-class="link--active" exact>Home</router-link>
       <router-link :to="{ name: 'user-id-profile', params: { id: 1 }, query: { lang: 'ja', page: 2 }, hash: '#next-user' }" class="link" active-class="link--active" exact>Users</router-link>
     </nav>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.state.count ++;
+    },
+    decrement(){
+      this.$store.state.count --;
+    }
+  }
+}
+</script>
 
 <style scoped>
 .link {
--- part3/src/views/Home.vue
+++ part3/src/views/Home.vue
@@ -7,11 +7,19 @@
     <button @click="toUsers">
       ユーザーのページへ
     </button>
+    <p>
+      {{ count }}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
   methods: {
     toUsers() {
       // this.$router.push('/users')
