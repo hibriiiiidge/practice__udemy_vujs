f8cc79f4baf1c11b2e609830d069943a8725142a
Yasunori Takahashi <hibriiiiidge@gmail.com>
2020-04-26 15:41:37 +0900

    section-250

--- part4/src/store/index.js
+++ part4/src/store/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from '../axios-auth'
 import router from '../router'
+import axiosRefresh from '../axios-refresh'
+
 
 Vue.use(Vuex)
 
@@ -18,7 +20,7 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    login({ commit }, authDate) {
+    login({ commit, dispatch }, authDate) {
       axios.post(
         '/accounts:signInWithPassword?key=AIzaSyC4c1SackQXBWDO3Y3zNK-Q-2tYrQxtwvc', {
           email: authDate.email,
@@ -27,7 +29,23 @@ export default new Vuex.Store({
         }
       ).then(res => {
         commit('updateIdToken', res.data.idToken)
+        setTimeout(() => {
+          dispatch('refreshIdToken', res.data.refreshToken)
+        }, res.data.expiresIn * 1000)
         router.push('/')
+      })
+    },
+    refreshIdToken({ commit, dispatch }, refreshToken) {
+      axiosRefresh.post(
+        'token?key=AIzaSyC4c1SackQXBWDO3Y3zNK-Q-2tYrQxtwvc', {
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken
+        }
+      ).then(res => {
+        commit('updateIdToken', res.data.id_token)
+        setTimeout(() => {
+          dispatch('refreshIdToken', res.data.refresh_token)
+        }, res.data.expires_in * 1000)
       })
     },
     register({ commit }, authDate) {
